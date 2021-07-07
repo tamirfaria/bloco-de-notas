45eff9e6983a15446ad3aa89e857dfb51f7d30f6
Lucas Tamir Faria <lucastamir@gmail.com>
2021-07-06 21:10:20 -0300

    feat: add AppWidget

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:bloco_de_notas/src/features/splash/splash_page.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Bloco de Notas | Raro Academy',
+      debugShowCheckedModeBanner: false,
+      home: SplashPage(),
+    );
+  }
+}
