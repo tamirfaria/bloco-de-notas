dfc5d9c90c7484552eca622993399450662349e4
Lucas Tamir Faria <lucastamir@gmail.com>
2021-07-06 21:11:03 -0300

    feat: add AppImages

---
+++ lib/src/shared/constants/app_images.dart
@@ -0,0 +1,7 @@
+class AppImages {
+  static const logoJornal = "assets/images/jornal.png";
+  static const logoNotes = "assets/images/notes_logo.png";
+  static const logoRaro = "assets/images/raro_academy_logo.png";
+  static const poweredBy = "assets/images/powered_by.png";
+  static const subtitle = "assets/images/subtitle.png";
+}
