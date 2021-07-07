ee48cdb8e22d62e60271a8fa0da5646b08f0bac8
Lucas Tamir Faria <lucastamir@gmail.com>
2021-07-07 20:38:56 -0300

    feat: add AnimatedCard at SplashPage

--- lib/src/features/splash/splash_page.dart
+++ lib/src/features/splash/splash_page.dart
@@ -1,59 +1,94 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
+import 'package:animated_card/animated_card.dart';
+import 'package:flutter/material.dart';
 import 'package:bloco_de_notas/src/shared/constants/app_colors.dart';
 import 'package:bloco_de_notas/src/shared/constants/app_images.dart';
-import 'package:flutter/material.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
-  _SplashPageState createState() => _SplashPageState();
+  createState() => _SplashPageState();
 }
 
 class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Container(
+    return Scaffold(
+      body: Container(
         height: double.infinity,
         width: double.infinity,
         decoration: const BoxDecoration(
           gradient: AppColors.blueGradient,
         ),
         child: Stack(
+          alignment: Alignment.center,
           children: [
-            Align(
-              alignment: const Alignment(
-                0.0,
-                -0.18,
-              ),
-              child: Image.asset(AppImages.logoNotes),
-            ),
-            Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
+            AnimatedCard(
+              direction: AnimatedCardDirection.top,
+              duration: const Duration(seconds: 1),
+              child: Stack(
+                alignment: Alignment.center,
                 children: [
-                  const Text(
-                    "journal",
-                    style: TextStyle(
-                      fontSize: 72,
-                      fontWeight: FontWeight.w700,
-                      color: Colors.white,
-                      fontFamily: "Montserrant",
-                      letterSpacing: -0.05,
-                    ),
+                  Positioned(
+                    top: (MediaQuery.of(context).size.height / 2) - 100,
+                    child: Image.asset(AppImages.logoNotes),
                   ),
-                  const Text(
-                    "YOUR ACADEMY'S BEST FRIEND",
-                    style: TextStyle(
-                      fontSize: 13.0,
-                      fontWeight: FontWeight.w300,
-                      color: Colors.white,
-                      fontFamily: "Montserrat",
-                      letterSpacing: 0.165,
+                  Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Text(
+                          "journal",
+                          style: TextStyle(
+                            fontSize: 72.0,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white,
+                            fontFamily: "Montserrat",
+                            letterSpacing: -0.05,
+                          ),
+                        ),
+                        const Text(
+                          "YOUR ACADEMY'S BEST FRIEND",
+                          style: TextStyle(
+                            fontSize: 13.0,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white,
+                            fontFamily: "Montserrat",
+                            letterSpacing: 0.165,
+                          ),
+                          textAlign: TextAlign.end,
+                        ),
+                      ],
                     ),
                   ),
                 ],
+              ),
+            ),
+            AnimatedCard(
+              direction: AnimatedCardDirection.bottom,
+              duration: const Duration(seconds: 1),
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text(
+                      "powered by",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        fontSize: 12.0,
+                        fontStyle: FontStyle.italic,
+                        fontFamily: "Roboto",
+                      ),
+                    ),
+                    const SizedBox(height: 8.0),
+                    Image.asset(AppImages.logoRaro),
+                    const SizedBox(height: 40.0),
+                  ],
+                ),
               ),
             ),
           ],
