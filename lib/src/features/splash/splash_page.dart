a44a86c50a0835240b5a9f8ff0f5e1869e5f5153
Lucas Tamir Faria <lucastamir@gmail.com>
2021-07-07 21:23:39 -0300

    feat: add Hero at SplashPage

--- lib/src/features/splash/splash_page.dart
+++ lib/src/features/splash/splash_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
-import 'package:animated_card/animated_card.dart';
+import 'package:bloco_de_notas/src/features/home/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_card/animated_card.dart';
 import 'package:bloco_de_notas/src/shared/constants/app_colors.dart';
 import 'package:bloco_de_notas/src/shared/constants/app_images.dart';
 
@@ -8,10 +9,26 @@ class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
-  createState() => _SplashPageState();
+  _SplashPageState createState() => _SplashPageState();
 }
 
 class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    WidgetsFlutterBinding.ensureInitialized().addPostFrameCallback(
+      (timeStamp) {
+        Future.delayed(const Duration(seconds: 2)).then(
+          (value) => Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (BuildContext context) => const HomePage(),
+            ),
+          ),
+        );
+      },
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,20 +49,29 @@ class _SplashPageState extends State<SplashPage> {
                 children: [
                   Positioned(
                     top: (MediaQuery.of(context).size.height / 2) - 100,
-                    child: Image.asset(AppImages.logoNotes),
+                    child: Hero(
+                      tag: "notes_logo",
+                      child: Image.asset(AppImages.logoNotes),
+                    ),
                   ),
                   Center(
                     child: Column(
                       mainAxisSize: MainAxisSize.min,
                       children: [
-                        const Text(
-                          "journal",
-                          style: TextStyle(
-                            fontSize: 72.0,
-                            fontWeight: FontWeight.w700,
-                            color: Colors.white,
-                            fontFamily: "Montserrat",
-                            letterSpacing: -0.05,
+                        const Hero(
+                          tag: "journal",
+                          child: Material(
+                            color: Colors.transparent,
+                            child: Text(
+                              "journal",
+                              style: TextStyle(
+                                fontSize: 72.0,
+                                fontWeight: FontWeight.w700,
+                                color: Colors.white,
+                                fontFamily: "Montserrat",
+                                letterSpacing: -0.05,
+                              ),
+                            ),
                           ),
                         ),
                         const Text(
