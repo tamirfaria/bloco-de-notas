bc3b8cbc5678a4a507ef9f8d3784e2a40a03027c
Lucas Tamir Faria <lucastamir@gmail.com>
2021-07-08 21:46:39 -0300

    feat: created const of boy image

--- lib/src/shared/constants/app_images.dart
+++ lib/src/shared/constants/app_images.dart
@@ -4,4 +4,5 @@ class AppImages {
   static const logoRaro = "assets/images/raro_academy_logo.png";
   static const poweredBy = "assets/images/powered_by.png";
   static const subtitle = "assets/images/subtitle.png";
+  static const boy = "assets/images/boy.png";
 }
