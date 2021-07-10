bf82d8d485d7e78e0a338ec34abd2a8128f77c19
Lucas Tamir Faria <lucastamir@gmail.com>
2021-07-09 21:06:54 -0300

    fix: remove const images

--- lib/src/shared/constants/app_images.dart
+++ lib/src/shared/constants/app_images.dart
@@ -1,8 +1,6 @@
 class AppImages {
-  static const logoJornal = "assets/images/jornal.png";
   static const logoNotes = "assets/images/notes_logo.png";
   static const logoRaro = "assets/images/raro_academy_logo.png";
-  static const poweredBy = "assets/images/powered_by.png";
-  static const subtitle = "assets/images/subtitle.png";
   static const boy = "assets/images/boy.png";
+  static const journalNotesPage = "assets/images/journal_notes_page.png";
 }
