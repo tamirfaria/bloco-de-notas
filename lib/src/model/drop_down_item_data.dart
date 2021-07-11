c9c0f60f4697fdc8a5bed2f387540014afe30dad
Lucas Tamir Faria <lucastamir@gmail.com>
2021-07-11 20:11:46 -0300

    feat: add DropDownItemData class

---
+++ lib/src/model/drop_down_item_data.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class DropDownItemData {
+  final Color color;
+  final String value;
+
+  const DropDownItemData({
+    required this.color,
+    required this.value,
+  });
+}
